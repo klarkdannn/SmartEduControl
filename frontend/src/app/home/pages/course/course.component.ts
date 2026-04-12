128294680755d94ab6c4a2a8cb2960df185d4333
klark_dan <daniil_larkovich@mail.ru>
2026-04-12 16:19:12 +0500

    Course from My Subjects

--- frontend/src/app/home/pages/course/course.component.ts
+++ frontend/src/app/home/pages/course/course.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectService } from './services/subject.service';
+import { SubjectResponse } from './models/subject.model';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  allSubjects: SubjectResponse[] = []; // Сюда упадут данные из базы
 
-  constructor() { }
+  constructor(private subjectService: SubjectService) {}
 
   ngOnInit(): void {
+    this.loadSubjects();
   }
 
+  loadSubjects(): void {
+    // Вызываем метод сервиса, который стучится в subject-service/api/subjects
+    this.subjectService.getAllSubjects().subscribe({
+      next: (data: SubjectResponse[]) => {
+        this.allSubjects = data;
+        console.log('Предметы загружены:', data);
+      },
+      error: (err: any) => console.error('Ошибка загрузки предметов:', err)
+    });
+  }
 }
