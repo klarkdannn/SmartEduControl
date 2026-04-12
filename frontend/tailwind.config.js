6bd452c30c9b48d4ce9a0852023101f996f63d24
klark_dan <daniil_larkovich@mail.ru>
2026-04-12 12:36:10 +0500

    New products

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e1e1e1",
+      },
+    },
+  },
+  plugins: [],
+  important: true,
+};
